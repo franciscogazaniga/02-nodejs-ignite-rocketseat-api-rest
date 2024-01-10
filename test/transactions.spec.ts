cd811a039c2e2e602c7a0fdcd1a1e1dbd7f028d6
Francisco Eduardo <franciscoeduardo@MacBook-Air-de-Francisco.local>
2024-01-09 21:05:18 -0300

    Created Transactions routes cateogry in transactions test file

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,21 +1,23 @@
-import { expect, test, beforeAll, afterAll } from 'vitest'
+import { expect, it, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-beforeAll(async () => {
-  await app.ready()
-})
-
-afterAll(async () => {
-  await app.close()
-})
-
-test('User can create a new transaction', async () => {
-  const response = await request(app.server).post('/transactions').send({
-    title: 'New transaction',
-    amount: 5000,
-    type: 'credit',
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
   })
 
-  expect(response.statusCode).toEqual(201)
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new transaction', async () => {
+    const response = await request(app.server).post('/transactions').send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
