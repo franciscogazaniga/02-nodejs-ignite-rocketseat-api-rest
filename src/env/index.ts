5e61af41cdd14d9be27de37a1525a65ddf6ca1ae
Francisco Eduardo <franciscoeduardo@MacBook-Air-de-Francisco.local>
2024-01-11 20:57:19 -0300

    .env.test created

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,11 +1,23 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+console.log(process.env.NODE_ENV)
+
+if (process.env.NODE_ENV === 'test') {
+  console.log('ENTROU NO AMBIENTE DE TESTE')
+  config({ path: '.env.test' })
+} else {
+  console.log('AMBIENTE DE PRODUÇÃO')
+  config()
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
   PORT: z.number().default(3333),
 })
+
+console.log(process.env.DATABASE_URL, process.env.NODE_ENV)
 
 const _env = envSchema.safeParse(process.env)
 
